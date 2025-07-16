fc74c3165f9da27bf26f46e3ab6a3e74a901c250
Karol <karolpilarski04@gmail.com>
2025-07-16 13:21:05 +0200

    Added display of new tasks

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let tasks = []
 
 class Task {
     constructor(description, date, priority) {
-        this.id = Date.now().toString();
         this.description = description;
         this.date = date;
         this.priority = priority;
@@ -13,15 +12,39 @@ const taskForm = document.getElementById("js--task-form-id");
 const taskDescription = document.getElementById("js--task-description-id");
 const taskDueDate = document.getElementById("js--task-due-date-id");
 const taskPriority = document.getElementById("js--task-priority-id");
+const tasksList = document.getElementById("js--task-list-id");
 
+function renderTasks() {
+    tasksList.innerHTML = "";
+    
+    tasks.forEach(task => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            ${task.description} - Priority: ${task.priority} Date: ${task.date}
+        `;
+        tasksList.appendChild(listItem);
+    });
+};
 
 taskForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    newTask = new Task(taskDescription.value, taskDueDate.value, taskPriority.value);
+    if (taskDescription.value.trim() === "") {
+        alert("Task description is empty")
+    } else if (taskDueDate.value === "") {
+        alert("Task date is empty")
+    } else {
+        const newTask = new Task(
+            taskDescription.value, 
+            taskDueDate.value, 
+            taskPriority.value
+        );
 
-    tasks.push(newTask);
+        tasks.push(newTask);
 
-    taskForm.reset();
-    console.log(tasks);
+        taskForm.reset();
+        console.log(tasks);
+
+        renderTasks()
+    }
 });
