40ba4263958f061361e3c58245c5f4ba7bec6d45
Karol <karolpilarski04@gmail.com>
2025-07-16 12:43:37 +0200

    Tworzenie klas i dodawanie ich do listy

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let tasks = []
 
-class task {
+class Task {
     constructor(description, date, priority) {
         this.id = Date.now().toString();
         this.description = description;
         this.date = date;
         this.priority = priority;
-        this.isCompleted = false;
     }
+};
 
-    markAsCompleted() {
-        this.isCompleted = true;
-    }
-}
+const taskForm = document.getElementById("js--task-form-id");
+const taskDescription = document.getElementById("js--task-description-id");
+const taskDueDate = document.getElementById("js--task-due-date-id");
+const taskPriority = document.getElementById("js--task-priority-id");
+
+
+taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    newTask = new Task(taskDescription.value, taskDueDate.value, taskPriority.value);
+
+    tasks.push(newTask);
+
+    taskForm.reset();
+    console.log(tasks);
+});
