a43e2211c9bd18719119709468f82e895bbf9857
Karol <karolpilarski04@gmail.com>
2025-07-17 22:12:54 +0200

    feat: Display empty state message when task list is empty.

--- script.js
+++ script.js
@@ -22,79 +22,86 @@ const taskDescription = document.getElementById("js--task-description-id");
 const taskDueDate = document.getElementById("js--task-due-date-id");
 const taskPriority = document.getElementById("js--task-priority-id");
 const tasksList = document.getElementById("js--task-list-id");
+const emptyListMessage = document.getElementById("js--empty-list-message");
 
 function renderTasks() {
     tasksList.innerHTML = "";
 
-    tasks.forEach(task => {
-        const listItem = document.createElement('li'); 
-        listItem.dataset.id = task.id;     
-        listItem.draggable = true;
-        
-        if (task.isCompleted) {
-            listItem.classList.add('completed');
-        }
+    if (tasks.length === 0) {
+        emptyListMessage.style.display = 'block';
+    } else {
+        emptyListMessage.style.display = 'none';
 
-        switch (task.priority) {
-            case 'Low':
-                listItem.classList.add('priority-low');
-                break;
-            case 'Medium':
-                listItem.classList.add('priority-medium');
-                break;
-            case 'High':
-                listItem.classList.add('priority-high');
-                break;
-        }
-
-
-        if (task.isEditing) {
-            listItem.innerHTML = `
-                <div class="left-li">
-                    <input type="text" class="edit-description-input" value="${task.description}">
-                    <input type="datetime-local" class="edit-date-input" value="${task.date}">
-                    <select class="edit-priority-select">
-                        <option value="Low" ${task.priority === 'Low' ? 'selected' : ''}>Low</option>
-                        <option value="Medium" ${task.priority === 'Medium' ? 'selected' : ''}>Medium</option>
-                        <option value="High" ${task.priority === 'High' ? 'selected' : ''}>High</option>
-                    </select>
-                </div>
-                <div class="right-li">
-                    <button class="save-edit-btn">Save</button>
-                    <button class="cancel-edit-btn">Cancel</button>
-                </div>
-            `
-        } else {
-            const taskDateObj = new Date(task.date);
+        tasks.forEach(task => {
+            const listItem = document.createElement('li'); 
+            listItem.dataset.id = task.id;     
+            listItem.draggable = true;
             
-            const datePart = taskDateObj.toLocaleDateString('pl-PL', {
-                year: 'numeric',
-                month: 'numeric',
-                day: 'numeric'
-            });
+            if (task.isCompleted) {
+                listItem.classList.add('completed');
+            }
 
-            const timePart = taskDateObj.toLocaleTimeString('pl-PL', {
-                hour: '2-digit',
-                minute: '2-digit'
-            });
+            switch (task.priority) {
+                case 'Low':
+                    listItem.classList.add('priority-low');
+                    break;
+                case 'Medium':
+                    listItem.classList.add('priority-medium');
+                    break;
+                case 'High':
+                    listItem.classList.add('priority-high');
+                    break;
+            }
 
-            let formattedDate = ` ${datePart} ${timePart}`;
 
-            listItem.innerHTML = `
-                <div class="left-li">
-                    <input type="checkbox" class="complete-checkbox" ${task.isCompleted ? 'checked' : ''}>
-                    <span class="span-description">${task.description} - Priorytet: ${task.priority} Data: ${formattedDate}</span>
-                </div>
-                <div class="right-li">
-                    <button class="edit-task-btn">Edit task</button>
-                    <button class="delete-task-btn">Delete task</button>
-                </div>
-            `;
+            if (task.isEditing) {
+                listItem.innerHTML = `
+                    <div class="left-li">
+                        <input type="text" class="edit-description-input" value="${task.description}">
+                        <input type="datetime-local" class="edit-date-input" value="${task.date}">
+                        <select class="edit-priority-select">
+                            <option value="Low" ${task.priority === 'Low' ? 'selected' : ''}>Low</option>
+                            <option value="Medium" ${task.priority === 'Medium' ? 'selected' : ''}>Medium</option>
+                            <option value="High" ${task.priority === 'High' ? 'selected' : ''}>High</option>
+                        </select>
+                    </div>
+                    <div class="right-li">
+                        <button class="save-edit-btn">Save</button>
+                        <button class="cancel-edit-btn">Cancel</button>
+                    </div>
+                `
+            } else {
+                const taskDateObj = new Date(task.date);
+                
+                const datePart = taskDateObj.toLocaleDateString('pl-PL', {
+                    year: 'numeric',
+                    month: 'numeric',
+                    day: 'numeric'
+                });
 
-        }
+                const timePart = taskDateObj.toLocaleTimeString('pl-PL', {
+                    hour: '2-digit',
+                    minute: '2-digit'
+                });
+
+                let formattedDate = ` ${datePart} ${timePart}`;
+
+                listItem.innerHTML = `
+                    <div class="left-li">
+                        <input type="checkbox" class="complete-checkbox" ${task.isCompleted ? 'checked' : ''}>
+                        <span class="span-description">${task.description} - Priorytet: ${task.priority} Data: ${formattedDate}</span>
+                    </div>
+                    <div class="right-li">
+                        <button class="edit-task-btn">Edit task</button>
+                        <button class="delete-task-btn">Delete task</button>
+                    </div>
+                `;
+
+            }
         
-        tasksList.appendChild(listItem);
-    });
+            tasksList.appendChild(listItem);
+        });
+    }
 };
 
 renderTasks();
